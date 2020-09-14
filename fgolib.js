7a5627c7cb7b7faba36a753ba15aa9f8e4246a76
RichardNixon4Prez <samdiode2@gmail.com>
2020-09-14 01:07:18 -0500

    Update fgolib.js

--- fgolib.js
+++ fgolib.js
@@ -1,2 +1,7 @@
 var Servants = {}
-Servants["sieg"] = {attack: 8394, Skill1: [], Skill2: [], Skill3: []};
+Servants["sieg"] = {attack: 8394,q:1,a:2,b:2,qh:3,ah:2,np_perhit:.78,np_type:"arts",np_hits:3,np_damage:[450,600,675,712.5,750],np_effects:[["def_down",3,20]],
+                    skill1: {name:"Artificial Hero (Fake) B+",icon:"np_gen",effect:[[["np_gen",3,20]],[["np_gen",3,21]],[["np_gen",3,22]],[["np_gen",3,20]],[["np_gen",3,20]],[["np_gen",3,20]],[["np_gen",3,20]],[["np_gen",3,20]],[["np_gen",3,20]],[["np_gen",3,20]]]},
+                    skill2: [name:"Magecraft C",icon:"arts",effect:[["arts",1,22]],[["arts",1,23.4]],[["arts",1,24.8]],[["arts",1,26.2]],[["arts",1,27.6]],[["arts",1,29]],[["arts",1,30.4]],[["arts",1,31.8]],[["arts",1,33.2]],[["arts",1,36]]],
+                    skill3: {name:"Dead-Count Shapeshifter EX",icon:"divinity",effect:[[["np_gauge",0,20],["dragon_dam",1,50]],[["np_gauge",0,21],["dragon_dam",1,55]],[["np_gauge",0,22],["dragon_dam",1,60]],[["np_gauge",0,23],["dragon_dam",1,65]],[["np_gauge",0,24],["dragon_dam",1,70]],
+                            [["np_gauge",0,25],["dragon_dam",1,75]],[["np_gauge",0,26],["dragon_dam",1,80]],[["np_gauge",0,27],["dragon_dam",1,85]],[["np_gauge",0,28],["dragon_dam",1,90]],[["np_gauge",0,30],["dragon_dam",1,100]]]}
+};
